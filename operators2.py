e3be29aaf8f3b8a4f12499fda4b8602de9a9911b
Sulemanhussainshah-SI <sulemanhussainshah78@gmail.com>
2026-07-12 09:34:22 +0530

    added operator.py

---
+++ operators2.py
@@ -0,0 +1,24 @@
+# Arrithematic operators
+
+a = 10
+b = 4
+
+print(a+b) # add
+print(a-b) #subtract
+print(a*b) #multiply
+print(a/b) #division
+print(a**b) #exponent
+print(a//b) #floor division
+print(a%b) #remainder
+
+# Comparision Operator ; always return value in boolean 
+
+x = 5
+y = 7
+ 
+print(x==y) # false 
+print(x!=y) #true
+print(x<y)  #true
+print(x>y)  #false
+print(x<=y) #true
+print(x>=y) #false
