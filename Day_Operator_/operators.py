2e0b8934ae006acf9b0a9813b2414fc13c7f8884
Sulemanhussainshah-SI <sulemanhussainshah78@gmail.com>
2026-09-10 17:02:02 +0530

    Add files via upload

---
+++ Day_Operator_/operators.py
@@ -0,0 +1,39 @@
+# Arithmetic operator
+
+a =3
+b=4
+c=a+b
+print(c)
+c=a-b
+print(c)
+
+# Assignment operators
+
+a=4-2 #assign 4-2 in a
+print(a)
+b=6
+"""b+=3 #increment the value of b by 3 and then assign it to b
+print(b)"""
+b-=3 #decrement the value of b by 3 and then assign it to b
+print(b) 
+ 
+ #Comparision operator
+
+""" Comparision Operator:- always the return value of boolean (true , False)"""
+s=5<4
+print(s)
+
+f = 5>=5
+print(f)
+
+g = 5<=5
+print(g)
+
+h = 5!=4
+print(h)
+ 
+i = 5!=5
+print(i)
+
+j = 5==5
+print(j)
